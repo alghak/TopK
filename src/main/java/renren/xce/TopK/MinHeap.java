ee68569ae4003673e5da54e22745f6e6d71ca0de
Xiaojie Zhang <zxj.xing@gmail.com>
2012-12-16 14:58:07 +0800

    add more comments.

--- src/main/java/renren/xce/TopK/MinHeap.java
+++ src/main/java/renren/xce/TopK/MinHeap.java
@@ -1,10 +1,12 @@
 package renren.xce.TopK;
 
 /**
- * Min-Heap implementation.
+ * MinHeap implementation.
+ * In MinHeap, the parent node is always larger than or equal to its child node.
+ * Note that MinHeap can't be empty, i.e, it must hold at least one elements.
+ * @author xiaojie.zhang
  *
  */
-
 public class MinHeap 
 {
     private long[] array_;
@@ -23,7 +25,11 @@ public class MinHeap
         init(array);
     }
     /**
-     * Initialize minHeap. Put elements into its array; Set heapSize.
+     * Initialize minHeap. 
+     * 1. Put elements into its array; 
+     * 2. Set heapSize.<p>
+     * Note that we can't initialize minHeap using a null array or empty array,
+     * or MinHeapException will be threw.
      * @param array  using array to initialize internal array_(deep copy).
      * @throws MinHeapException 
      */
@@ -52,7 +58,7 @@ public class MinHeap
         return array;
     }
     /**
-     * Maintains the min-heap property of heap rooted at index.
+     * Maintains the min-heap property of heap rooted at index.<p>
      * Assumption: sub-heap rooted at left and right child of index
      * is all min-heap, but not the heap rooted at index.
      * @param index the root of heap that need to maintain heap property.
@@ -73,40 +79,50 @@ public class MinHeap
             least = right;
         }
         if (least != index) {
-//            long temp = array_[index];
-//            array_[index] = array_[least];
-//            array_[least] = temp;
             exchange(index, least);
             maintainHeapProperty(least);
         }
     }
-
+    /**
+     * Build MinHeap. Make every node in MinHeap conform the MinHeap Property: parent(node) >= node.
+     *
+     */
     public void buildHeap() {
         for (int i = heapSize_ / 2; i >= 0; --i) {
             maintainHeapProperty(i);
         }
     }
-
+    /**
+     * Sort elements in MinHeap.
+     */
     public void sort() {
         buildHeap();
         for (int i = array_.length -1; i >= 1; --i) {
-//            long temp = array_[0];
-//            array_[0] = array_[i];
-//            array_[i] = temp;
             exchange(0, i);
             --heapSize_;
             maintainHeapProperty(0);
         }
     }
-
+    /**
+     * Get the minimum elements in MinHeap.
+     * This operation is trivial, just return the root of MinHeap.
+     * @return the minimum elements in MinHeap.
+     */
     public long min() {
         return array_[0];
     }
-
+    /**
+     * Replace the minimum element with l, may violate the MinHeap property.
+     * @param l the value to replace MinHeap root.
+     */
     public void replaceMin(long l) {
         array_[0] = l;
     }
-
+    /**
+     * Exchange array_[i] and array[j].
+     * @param i
+     * @param j
+     */
     void exchange(int i, int j) {
         if (i >= array_.length ||
                 j >= array_.length) {
@@ -116,19 +132,37 @@ public class MinHeap
         array_[i] = array_[j];
         array_[j] = temp;
     }
-    
+    /**
+     * Get parent index of node i.
+     * Note that index in MinHeap is based on zero.
+     * @param i
+     * @return
+     */
     static int parentIndex(int i) {
         return (i - 1) / 2;
     }
-
+    /**
+     * Get left child index of node i.
+     * @param i
+     * @return
+     */
     static int leftChildIndex(int i) {
         return i * 2 + 1;
     }
-
+    /**
+     * Get right child index of node i.
+     * @param i
+     * @return
+     */
     static int rightChildIndex(int i) {
         return (i + 1) * 2;
     }
-    
+    /**
+     * MinHeapException is used to indicate MinHeap's internal error, such
+     * as null array_, empty array_.
+     * @author xiaojie.zhang
+     *
+     */
     public class MinHeapException extends Exception {
         private static final long serialVersionUID = 1L;
 
